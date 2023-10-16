cd4d5f0ad2fed47a5fd5b6a4cb8abc88b0294409
Lahiru-Madhuwantha <lahirumadhuwantha293@gmail.com>
2023-10-16 21:12:22 +0530

    12

--- src/routes/products.js
+++ src/routes/products.js
@@ -3,10 +3,12 @@ const ProductController = require('../controllers/products');
 
 const router = express.Router();
 
+const verifyToken = require('../middlewares/verifyToken');
+
 router.post('/',ProductController.create);
 router.put('/:id',ProductController.update)
 router.delete('/:id',ProductController.delete)
-router.get('/',ProductController.getAll)
+router.get('/all', verifyToken ,ProductController.getAll)
 router.get('/:id',ProductController.getOne)
 router.get('searchResults',ProductController.search)
 
